40ae883f50f9d7cb6869a23369d42a126b9d226b
condrowiyono <condro@outlook.co.id>
2019-06-24 21:07:09 +0000

    qrstream reader

--- pages/index.vue
+++ pages/index.vue
@@ -31,12 +31,19 @@
           </v-list-tile>
         </v-list>
       </div>
+      <div v-if="tab = 'scan'">
+        <p class="error">{{ error }}</p>
+        <v-container>
+          <b>RESULT : {{result}} </b>
+          <qrcode-stream @decode="onDecode" @init="onInit" />
+        </v-container>
+      </div>
     </v-flex>
     <v-bottom-nav
       :active.sync="tab"
       :value="true"
-      absolute
-      color="transparent"
+      fixed
+      color="#fff"
     >
       <v-btn
         color="blue"
@@ -80,18 +87,44 @@
 </template>
 
 <script>
+import { QrcodeStream } from 'vue-qrcode-reader';
 
 export default {
+  components: { QrcodeStream },
   data () {
     return {
       tab: (this.$route.query.tab) ? this.$route.query.tab : 'home',
       title: (this.$route.query.tab) ? this.$route.query.tab : 'Beranda',
+      result: '',
+      error: ''
     }
   },
   methods: {
     async logout() {
       await this.$auth.logout();
     },
+    onDecode(result) {
+      this.result = result;
+    },
+    async onInit (promise) {
+      try {
+        await promise
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          this.error = "ERROR: you need to grant camera access permisson"
+        } else if (error.name === 'NotFoundError') {
+          this.error = "ERROR: no camera on this device"
+        } else if (error.name === 'NotSupportedError') {
+          this.error = "ERROR: secure context required (HTTPS, localhost)"
+        } else if (error.name === 'NotReadableError') {
+          this.error = "ERROR: is the camera already in use?"
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = "ERROR: installed cameras are not suitable"
+        } else if (error.name === 'StreamApiNotSupportedError') {
+          this.error = "ERROR: Stream API is not supported in this browser"
+        }
+      }
+    }
   },
 }
 </script>
